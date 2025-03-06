fe604732b835b7547923172fab53653937edde4e
Muhammad Ali Asif <mohammadali6918773@gmail.com>
2025-03-06 13:11:49 +0500

    changes

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+    }
+}
